2c30052eed935df7eea7cef2a56a4a8e6a236ac4
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-12-02 16:59:49 -0800

    Added finished database utility and insert
    Added spreadsheets with the questions and their correct answers

---
+++ Database_Connection/src/database_stuff/DatabaseTests.java
@@ -0,0 +1,68 @@
+package database_stuff;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import questions.Question;
+
+public class DatabaseTests
+{
+	
+	DatabaseUtility test;
+	DatabaseInsert test1;
+	int id = 30;
+	
+	@Before
+	public void setUp()
+	{
+		test = new DatabaseUtility();
+		test1 = new DatabaseInsert();
+	}
+	
+	@Test
+	public void testInsertTrueFalse()
+	{
+		assertTrue(test1.insertQuestion("Will it rain today", "t"));
+		assertFalse(test1.insertQuestion(null, "t"));
+	}
+	
+	@Test
+	public void testInsertShortAnswer()
+	{
+		assertTrue(test1.insertQuestion("Will it rain today", "Maybe, it could", "maybe,probably"));
+		assertFalse(test1.insertQuestion("What is my name", null, null));
+	}
+	
+	@Test
+	public void testInsertMultipleChoice()
+	{
+		assertTrue(test1.insertQuestion("How many children are in Mary Poppins", "2", "55", "2", "7", "25"));
+		assertFalse(test1.insertQuestion("What is my name", "3", "Nick", "Fish", null, "Nothing"));
+	}
+	
+	//Will fail on first run if the database is empty
+	@Test
+	public void testretrieveQuestions()
+	{
+		Question q1 = test.retrieveQuestion("shortanswer");
+		Question q2 = test.retrieveQuestion("truefalse");
+		Question q3 = test.retrieveQuestion("multiplechoice");
+		assertFalse(q1.getError());
+		assertFalse(q2.getError());
+		assertFalse(q3.getError());
+	}
+	
+	@Test
+	public void testResetFlags()
+	{
+		assertTrue(test.resetFlags("multiplechoice"));
+		assertTrue(test.resetFlags("shortanswer"));
+		assertTrue(test.resetFlags("truefalse"));
+		assertFalse(test.resetFlags("test"));
+	}
+
+	
+}
---
+++ Database_Connection/src/questions/TrueFalseQuestion.java
@@ -0,0 +1,71 @@
+package questions;
+
+/**
+ * TrueFalseQuestion.java
+ * Author: Jenia Rousseva
+ * Revision: 2, Jenia Rousseva
+ * Date: 11/20/2015
+ * This file provides the implementation of a true/false question object.
+ * For these types of questions the user must answer T or F and no hints 
+ * are allowed as of now.
+ * Reference:
+ * 
+ * http://www.regexplanet.com/advanced/java/index.html
+ */
+
+public class TrueFalseQuestion extends Question 
+{
+	private static final String REGEX = "((?i)[tf])";
+	
+	
+	/*
+	 * Initializes a new node with the default values. 
+	 * Calls superclass constructor and sets the appropriate
+	 * regex expression.
+	 */
+	
+	public TrueFalseQuestion()
+	{
+		super(null, null);
+		super.setPattern(REGEX);
+	}//end TrueFalseQuestion
+	
+	
+	/*
+	 * Initializes a new TrueFalseQuestion with the parameters passed in.
+	 * Parameters:
+	 * String question - The question prompt
+	 * String correctAnswer - The right answer for the question
+	 */
+	
+	public TrueFalseQuestion(String question, String correctAnswer) 
+	{
+		super(question, correctAnswer);
+		super.setPattern(REGEX);
+	}//end TrueFalseQuestion
+	
+	
+	/*
+	 * Overridden method from the Question superclass to display the
+	 * question and a prompt to enter T or F.
+	 */
+	
+	@Override
+	public void printQuestion()
+	{		
+		super.printQuestion();
+		System.out.println("\n\nChoose T or F:\n");
+	}//end printQuestion
+	
+	
+	/*
+	 * Overridden method to provide a hint for a true/false question.
+	 */
+
+	@Override
+	public void hint() // Allow hints here?
+	{
+		// TODO Auto-generated method stub
+	}//end hint
+
+}//end TrueFalseQuestion
